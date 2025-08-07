2d6efd8ec0be1b8292b737eb620ab420645f5ff9
Penn Wang <guyusay@gmail.com>
2025-08-07 13:46:32 +0800

    ci: set assetPrefix to the sub path

--- packages/react-plain-editor/rsbuild.config.ts
+++ packages/react-plain-editor/rsbuild.config.ts
@@ -3,4 +3,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: '/codemirror-lang-mustache/react-plain-editor', // Set this to your desired public path, e.g. '/static/' or './'
+  },
 });
